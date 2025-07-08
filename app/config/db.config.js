f27d84ee8a37e3e6e9111abca715091ddeb3ad5d
nitinbelectriq <nitin.gautam@belectriq.co>
2025-07-08 11:41:19 +0530

    nackend

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -3,18 +3,6 @@
 //--21-04-2025-main api DB--Development DB
 let msMain = {
 
-// HOST: "localhost",
-// USER: "root",
-// PASSWORD: "editor",
-// DB: "belectriq_cms",
-// PORT : 3306
-
-// HOST:"mysql-367fd85f-belectriq-be1a.i.aivencloud.com",
-// USER:"avnadmin",
-// PASSWORD:"AVNS_Z83CtBY_B5IGk4Bur4-",
-// DB:"AivenBelectriq",
-// PORT:24048
-
 
 HOST: "database-1.c9ws6aq42w98.ap-south-1.rds.amazonaws.com",
 USER: "admin",
@@ -26,13 +14,6 @@ PORT : 3306
 
 //-21-04-2025-Postgres Logs DB--Development DB for Aiven
 let pgPool = {
-  // HOST: "pg-26f3e5f8-belectriq-be1a.c.aivencloud.com", // private IP  
-  // USER: "avnadmin",
-  // PASSWORD: "AVNS_qfuof0AwEhqUa5goVON",
-  // DB: "belectriqOCPP",
-  // PORT: 24048,
-  // ssl: true,
-  // multipleStatements : true
 
   HOST: "belectriq-database-2.c9ws6aq42w98.ap-south-1.rds.amazonaws.com", // private IP  
   USER: "postgres",
