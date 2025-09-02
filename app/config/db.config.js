36ecba6fb61183a004a8af4c4774653ba412e4cf
nitinbelectriq <nitin.gautam@belectriq.co>
2025-09-02 16:46:14 +0530

    change databse

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,24 +1,11 @@
 
 
 //--21-04-2025-main api DB--Development DB
-let msMain = {
-
-
-// HOST: "database-1.c9ws6aq42w98.ap-south-1.rds.amazonaws.com",
-// USER: "admin",
-// PASSWORD: "Belectriq##$$%%##",
-// DB: "cms_belectriq",
-// PORT : 3306
-
-// HOST: "localhost",
-// USER: "root",
-// PASSWORD: "Nitin12345.",   
-// DB: "cms_belectriq_test",
-// PORT : 3306
-HOST: "aws-benz-mysql-database.cvymaesqwh1j.ap-south-1.rds.amazonaws.com",
+ let msMain = {
+HOST: "belectriq-cms-database-1-instance-1.cgdyye6ya8ia.us-east-1.rds.amazonaws.com",
 USER: "admin",
-PASSWORD: "Cubenzpower##$$%%##",
-DB: "cubenz_cms",
+PASSWORD: "Belectriq##$$%%##",
+DB: "belectriq_cms",
 PORT : 3306
 }
 
@@ -26,10 +13,10 @@ PORT : 3306
 //-21-04-2025-Postgres Logs DB--Development DB for Aiven
 let pgPool = {
 
- user: "postgres",
-    host: "cubenzpgadmin-1.cvymaesqwh1j.ap-south-1.rds.amazonaws.com",
-    database: "postgres",
-    password: "cubenz#$#$%&%&",
+user: "belectriq",
+    host: "cms-belectriqpgadmin-database-1.cgdyye6ya8ia.us-east-1.rds.amazonaws.com",
+    database: "belectriq_chargerlog",
+    password: "Belectriq##$$%%##",
     port: 5432,
     multipleStatements : true,
     ssl: { rejectUnauthorized: false }
